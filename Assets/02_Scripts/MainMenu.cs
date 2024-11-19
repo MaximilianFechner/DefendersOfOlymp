369bc4e80dfbcd8273ad352baaab4285d52757a4
Elija <eli.schmuck@gmx.net>
2024-11-19 23:51:05 +0100

    Main Menu
    Start, Quit, Options -> Quit Options Button, Fullscreen Button, Change Resolution (Work in progress)

---
+++ Assets/02_Scripts/MainMenu.cs
@@ -0,0 +1,69 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+public class MainMenu : MonoBehaviour
+{
+
+    //Play
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    //Quit
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Resolution
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    //Fullscreen
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+}
+
+
+
+
+
