8e3cbade6627ef0535252f2f841a529d03914a1d
Elija <eli.schmuck@gmx.net>
2024-11-20 14:24:09 +0100

    Main Menu v0.4
    Dropdown Menu fixed (few issues but it works), small fixes (canas)

--- Assets/02_Scripts/MainMenu.cs
+++ Assets/02_Scripts/MainMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEditor;
 public class MainMenu : MonoBehaviour
 {
 
@@ -16,44 +17,69 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
     }
 
     //Resolution
-    public Dropdown resolutionDropdown;
+    public Dropdown _resolutionDropdown;
     public GameObject resolution;
 
-    Resolution[] resolutions;
+    Resolution[] _resolutions;
 
     void Start()
     {
-        resolutionDropdown = resolution.GetComponent<Dropdown>();
-        resolutions = Screen.resolutions;
+        /* 
+         resolutionDropdown = resolution.GetComponent<Dropdown>();
+         resolutions = Screen.resolutions;
 
-        resolutionDropdown.ClearOptions();
+         resolutionDropdown.ClearOptions();
 
-        List<string> options = new List<string>();
+         List<string> options = new List<string>();
 
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
+         int currentResolutionIndex = 0;
+         for (int i = 0; i < resolutions.Length; i++)
+         {
+             string option = resolutions[i].width + "x" + resolutions[i].height;
+             options.Add(option);
+
+             if (resolutions[i].width == Screen.currentResolution.width &&
+                 resolutions[i].height == Screen.currentResolution.height)
+             {
+                 currentResolutionIndex = i;
+             }
+         }
+
+         resolutionDropdown.AddOptions(options);
+         resolutionDropdown.value = currentResolutionIndex;
+         resolutionDropdown.RefreshShownValue();
+        */
+
+        var options = new List<string>();
+        _resolutions = Screen.resolutions;
+        var currentResolutionIndex = 0;
+        for (var i = 0; i < _resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
+            var option = _resolutions[i].width + " x " + _resolutions[i].height;
             options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
         }
 
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        /*
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+        */
     }
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        var resolution = _resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
