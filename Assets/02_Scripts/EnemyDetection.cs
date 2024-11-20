0ea426e66f4702830c8b09f8de278cef8b30496d
Jan Pritzl <42317050+JanPritzl@users.noreply.github.com>
2024-11-20 14:25:20 +0100

    EnemyDetection und Angriff des Turms implementiert. Testgegner aufgebaut.

---
+++ Assets/02_Scripts/EnemyDetection.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+
+    [SerializeField] private float _radius;
+    [SerializeField] private LayerMask _enemyLayerMask;
+    [SerializeField] private TargetType _targetType;
+
+    [SerializeField] private float _timeToSpawn;
+    [SerializeField] private float _timer;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private GameObject _targetEnemy;
+
+    private enum TargetType {
+        lowestHealth,
+        highestHealth
+    }
+
+    private void Awake() {
+        CircleCollider2D circleCollider2D = GetComponent<CircleCollider2D>();
+        circleCollider2D.radius = _radius;
+
+        _timer = 0;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_targetEnemy != null && _timer >= _timeToSpawn) {
+            //TODO Hier müsste die Überprüfung kommen, ob der Gegner noch ein Projektil aushält,
+            //Wenn nicht, soll ein anderer Enemy ausgewählt werden.
+            SpawnProjectile(_targetEnemy);
+            _timer = 0;
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.tag.Equals("Enemy")) {
+            _targetEnemy = GetTargetEnemy();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        _targetEnemy = null;
+    }
+
+    private GameObject GetTargetEnemy() {
+        Collider2D[] enemiesColliders = Physics2D.OverlapCircleAll(transform.position, _radius, _enemyLayerMask);
+        _targetEnemy = null;
+        if (enemiesColliders.Length > 0) {
+            EnemyBehaviour targetEnemyBehaviour = enemiesColliders[0].GetComponent<EnemyBehaviour>();
+
+            switch (_targetType) {
+                case TargetType.lowestHealth:
+                    foreach (Collider2D collider2d in enemiesColliders) {
+                        EnemyBehaviour currentEnemy = collider2d.GetComponent<EnemyBehaviour>();
+                        if (currentEnemy.currentLife < targetEnemyBehaviour.currentLife) {
+                            targetEnemyBehaviour = currentEnemy;
+                        }
+                    }
+                    break;
+                case TargetType.highestHealth:
+                    foreach (Collider2D collider2d in enemiesColliders) {
+                        EnemyBehaviour currentEnemy = collider2d.GetComponent<EnemyBehaviour>();
+                        if (currentEnemy.currentLife > targetEnemyBehaviour.currentLife) {
+                            targetEnemyBehaviour = currentEnemy;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            _targetEnemy = targetEnemyBehaviour.gameObject;
+        }
+        return _targetEnemy;
+    }
+
+    private void SpawnProjectile(GameObject target) {
+        GameObject projectile = Instantiate(_projectilePrefab, _spawnPoint);
+        projectile.transform.SetParent(null);
+        ProjectileBehaviour projectileBehaviour = projectile.GetComponent<ProjectileBehaviour>();
+        projectileBehaviour.SetTargetEnemy(target);
+    }
+}
---
+++ Assets/02_Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _targetEnemy;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _damage;
+
+    private Rigidbody2D rb2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_targetEnemy != null) {
+            Vector2 direction = (_targetEnemy.transform.position - rb2D.transform.position).normalized;
+            rb2D.MovePosition(rb2D.position + direction * _movementSpeed * Time.fixedDeltaTime);
+        } else {
+            //TODO Diese Lösung ist nur temporär und muss entfernt werden,
+            //wenn Tower erkennt, dass keine Projektile mehr gespawned werden sollen, wenn Life unter 0
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetTargetEnemy(GameObject targetEnemy) {
+        this._targetEnemy = targetEnemy;    
+    }
+
+    public float GetDamage() {
+        return _damage; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag.Equals("Enemy") && collider.gameObject.Equals(_targetEnemy)) {
+            EnemyBehaviour enemy = collider.gameObject.GetComponent<EnemyBehaviour>();
+            enemy.LoseLife(_damage);
+            Destroy(gameObject);
+        }
+    }
+}
