195352d79ed0db1d48db34050cd4733722045ccd
Maximilian Fechner <iskalder.gg@gmail.com>
2024-12-02 17:53:18 +0100

    Card mechanic

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -4,17 +4,27 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-
     public int RemainingLives { get; private set; }
     public int RemainingEnemies { get; private set; }
     public int EnemiesKilled { get; private set; }
     public int waveNumber { get; private set; }
 
+    [Header("Game Design Values")]
+    [Tooltip("The lifes the player starts with")]
+    [Min(1)]
+    [SerializeField]
+    private int _playerStartLives = 5;
+
+    [Space(10)]
+    [Header("UI Elements")]
     public Text playerLifeText;
     public Text enemiesKilledText;
     public Text waveNumberText;
     public Text remainingEnemiesText;
+    public Button nextWaveButton;
+    public Button drawCardButton;
 
+    private int cardsToDraw = 1;
 
     private void Awake()
     {
@@ -31,7 +41,7 @@ public class GameManager : MonoBehaviour
     // only for tests in Unity, implement the NewGame()-Method for build game
     private void Start()
     {
-        RemainingLives = 5;
+        RemainingLives = _playerStartLives;
         EnemiesKilled = 0;
         waveNumber = 0;
         waveNumberText.text = $"Current wave: {waveNumber.ToString()}";
@@ -42,7 +52,7 @@ public class GameManager : MonoBehaviour
     public void NewGame()
     {
         EnemiesKilled = 0;
-        RemainingLives = 5;
+        RemainingLives = _playerStartLives;
         waveNumber = 0;
         waveNumberText.text = $"Current wave: {waveNumber.ToString()}";
         playerLifeText.text = RemainingLives.ToString();
@@ -71,6 +81,11 @@ public class GameManager : MonoBehaviour
     {
         RemainingEnemies--;
         remainingEnemiesText.text = $"Remaining Enemies: {RemainingEnemies.ToString()}";
+
+        if (RemainingEnemies == 0)
+        {
+            EndOfWave();
+        }
     }
 
     public void AddWaveCounter()
@@ -79,10 +94,20 @@ public class GameManager : MonoBehaviour
         waveNumberText.text = $"Current wave: {waveNumber.ToString()}";
     }
 
-
     private void GameOver()
     {
         Debug.Log("Game Over!");
+    }
+
+    private void EndOfWave()
+    {
+        int remainingCardsToDraw = cardsToDraw;
+        nextWaveButton.gameObject.SetActive(true);
+        if (remainingCardsToDraw > 0)
+        {
+            drawCardButton.gameObject.SetActive(true);
+            remainingCardsToDraw--;
+        }
     }
 
 
