e7f2ece2166f0e1ab24a6e7e527c746d66ddf07a
Maximilian Fechner <iskalder.gg@gmail.com>
2024-12-06 19:37:50 +0100

    * Visual Healthbar updates

--- Assets/02_Scripts/Enemy/EnemyHealthBar.cs
+++ Assets/02_Scripts/Enemy/EnemyHealthBar.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class EnemyHealthBar : MonoBehaviour
 {
+    public GameObject healtbarOutlines;
     public void SetHealth(float healthPercentage)
     {
-        this.gameObject.transform.localScale = new Vector3(healthPercentage * 5, 1, 1);
+        //this.gameObject.transform.localScale = new Vector3(healthPercentage * 5, transform.localScale.y, 1);
+        this.gameObject.transform.localScale = new Vector3(healthPercentage * 0.25f, transform.localScale.y, 1);
     }
     public void SetVisible(bool isVisible)
     {
         this.gameObject.SetActive(isVisible);
+        healtbarOutlines.SetActive(isVisible);
     }
 }
