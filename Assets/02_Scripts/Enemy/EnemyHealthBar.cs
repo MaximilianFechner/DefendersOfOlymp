8b8e25704f9e0274d5bcd03fcf7b126f551b663a
Maximilian Fechner <iskalder.gg@gmail.com>
2024-12-07 09:37:17 +0100

    * Centaur and Cerberus Animations update

--- Assets/02_Scripts/Enemy/EnemyHealthBar.cs
+++ Assets/02_Scripts/Enemy/EnemyHealthBar.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class EnemyHealthBar : MonoBehaviour
 {
     public GameObject healtbarOutlines;
+    private float scaleX;
+    private float scaleY;
+
+    private void Start()
+    {
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
+    }
+
     public void SetHealth(float healthPercentage)
     {
         //this.gameObject.transform.localScale = new Vector3(healthPercentage * 5, transform.localScale.y, 1);
-        this.gameObject.transform.localScale = new Vector3(healthPercentage * 0.25f, transform.localScale.y, 1);
+        this.gameObject.transform.localScale = new Vector3(healthPercentage * scaleX, scaleY, 1);
     }
     public void SetVisible(bool isVisible)
     {
