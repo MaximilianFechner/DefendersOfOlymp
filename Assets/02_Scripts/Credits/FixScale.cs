66f54cd8a6521ee079ff81cbd08bd4b244fbbddf
Maximilian Fechner <iskalder.gg@gmail.com>
2025-01-16 23:21:01 +0100

    Update FixScale.cs

--- Assets/02_Scripts/Credits/FixScale.cs
+++ Assets/02_Scripts/Credits/FixScale.cs
@@ -8,24 +8,20 @@ public class FixScale : MonoBehaviour
 
     private void Awake()
     {
-        // Speichere die ursprüngliche Skalierung und Rotation
         originalScale = transform.localScale;
         originalRotation = transform.rotation;
     }
 
     private void LateUpdate()
     {
-        // Skalierung des Elternobjekts abrufen
         Vector3 parentScale = transform.parent != null ? transform.parent.localScale : Vector3.one;
 
-        // Fixiere die Skalierung (nur X-Achse berücksichtigen)
         transform.localScale = new Vector3(
-            originalScale.x * Mathf.Sign(parentScale.x), // Spiegelung auf X-Achse berücksichtigen
-            originalScale.y,                             // Y-Skalierung bleibt unverändert
-            originalScale.z                              // Z-Skalierung bleibt unverändert
+            originalScale.x * Mathf.Sign(parentScale.x),
+            originalScale.y,
+            originalScale.z
         );
 
-        // Fixiere die Rotation (zurück zur Originalrotation)
         transform.rotation = originalRotation;
     }
 }
