96404b0a4c16670fc599300c05bcfebfbd46b95c
Elija <eli.schmuck@gmx.net>
2025-01-19 17:35:07 +0100

    Interactive Main Menu Final

--- Assets/02_Scripts/Cards/CardManager.cs
+++ Assets/02_Scripts/Cards/CardManager.cs
@@ -38,12 +38,15 @@ public class CardManager : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(1))
+        {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-            if (hit.collider != null) {
+            if (hit.collider != null)
+            {
                 GameObject tower = hit.collider.gameObject;
-                if (tower.tag.Equals("Tower")) {
+                if (tower.tag.Equals("Tower"))
+                {
                     BaseTower baseTower = tower.GetComponent<BaseTower>();
                     baseTower.SetTowerMenu();
                 }
@@ -64,14 +67,17 @@ public class CardManager : MonoBehaviour
 
         //TODO Der PreviewTower aka BuildingGhost, darf nicht angreifen. 
         Cards previewTower = GetCurrentCard();
-        
+
         PlacementPreview(previewTower);
         AudioManager.Instance.PlayCardSFX();
 
         PlacedObject placedObjectPreviewTower = previewTower.TowerPrefab.GetComponent<PlacedObject>();
-        if (placedObjectPreviewTower != null) {
+        if (placedObjectPreviewTower != null)
+        {
             GridBuildingSystem.Instance.RefreshSelectedObjectType(placedObjectPreviewTower.GetPlacedObjectTypeSO());
-        } else {
+        }
+        else
+        {
             Debug.LogError($"Cannot GetComponent<PlacedObject>() from previewTower {previewTower.ToString()}");
         }
 
@@ -97,10 +103,12 @@ public class CardManager : MonoBehaviour
         Cards selectedCard = GetCurrentCard();
         Instantiate(selectedCard.TowerPrefab, mousePosition, Quaternion.identity);
 
+        /*
         if (currentCard.TowerName.Contains("Zeus")) AudioManager.Instance.PlayTowerPlacementSFX(0);
         else if (currentCard.TowerName.Contains("Poseidon")) AudioManager.Instance.PlayTowerPlacementSFX(1);
         else if (currentCard.TowerName.Contains("Hera")) AudioManager.Instance.PlayTowerPlacementSFX(2);
         else if (currentCard.TowerName.Contains("Hephaistos")) AudioManager.Instance.PlayTowerPlacementSFX(3);
+        */
 
         if (currentPreview != null)
         {
@@ -110,11 +118,28 @@ public class CardManager : MonoBehaviour
 
         GridBuildingSystem.Instance.PlaceTower();
         currentPreview = null;
-        
+
         ClearCard();
 
         GameManager.Instance.StartNextWave();
     }
 
+    private void PlacementPreview(Cards currentCard)
+    {
+        if (currentCard == null) return;
+
+        if (currentPreview == null)
+        {
+            currentPreview = Instantiate(currentCard.TowerPrefab);
+            currentPreview.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+
+            // deactivate the ability of the preview to attack the enemies as preview tower
+            MonoBehaviour[] scripts = currentPreview.GetComponents<MonoBehaviour>();
+            foreach (MonoBehaviour script in scripts)
+            {
+                script.enabled = false;
+            }
+        }
+    }
 }
 
