a9caf3f7e701269289f7861258a7ec1df88576b8
Zarbony <jenskaerger95@gmail.com>
2025-01-31 12:10:50 +0100

    Fix Reference Bugs

--- Assets/02_Scripts/ActiveSkills/HephaistosQuake.cs
+++ Assets/02_Scripts/ActiveSkills/HephaistosQuake.cs
@@ -106,7 +106,7 @@ public class HephaistosQuake : MonoBehaviour
         {
             _cameraShake = FindFirstObjectByType<CameraShake>();
 
-            if (_cameraShake == null)
+            if (_cameraShake != null)
             {
                 Debug.LogError("CameraShake not found in the scene!");
             }
