6ed4ee66801a8d7ce836ab56bf9de35115f788ba
Maximilian Fechner <iskalder.gg@gmail.com>
2025-01-31 18:17:43 +0100

    Gridsystem

--- Assets/02_Scripts/GridSystem/GridBuildingSystem.cs
+++ Assets/02_Scripts/GridSystem/GridBuildingSystem.cs
@@ -11,23 +11,44 @@ public class GridBuildingSystem : MonoBehaviour
     public event EventHandler OnSelectedChanged;
     public event EventHandler OnObjectPlaced;
 
-    private Grid<GridObject> grid;
+    [System.Serializable]
+    public struct GridData
+    {
+        public int width;
+        public int height;
+        public float cellSize;
+        public Vector3 offset;
+        [HideInInspector] public Grid<GridObject> grid;
+    }
+
     [SerializeField] private List<PlacedObjectTypeSO> placedObjectTypeSOList;
     private PlacedObjectTypeSO placedObjectTypeSO;
     private PlacedObjectTypeSO.Dir dir = PlacedObjectTypeSO.Dir.Down;
 
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
-    [SerializeField] private float cellSize;
-
-
+    [SerializeField] private GridData mainGrid; // Daten für das Hauptgrid
+    [SerializeField] private GridData upperGrid;
+    [SerializeField] private GridData lowerGrid;
 
     private void Awake() {
         Instance = this;
 
-        grid = new Grid<GridObject>(gridWidth, gridHeight, cellSize, transform.position, (Grid<GridObject> g, int x, int z) => new GridObject(g, x, z));
+        // Initialisiere beide Grids
+        InitializeGrid(ref mainGrid);
+        InitializeGrid(ref upperGrid);
+        InitializeGrid(ref lowerGrid);
 
         placedObjectTypeSO = null;
+    }
+
+    private void InitializeGrid(ref GridData gridData)
+    {
+        gridData.grid = new Grid<GridObject>(
+            gridData.width,
+            gridData.height,
+            gridData.cellSize,
+            transform.position + gridData.offset,
+            (Grid<GridObject> g, int x, int z) => new GridObject(g, x, z)
+        );
     }
 
 
@@ -66,8 +87,12 @@ public class GridBuildingSystem : MonoBehaviour
         }
     }
 
-    public bool PlaceTower() {
-        grid.GetXY(GetMouseWorldPosition(), out int x, out int z);
+    public bool PlaceTower() 
+    {
+        GridData activeGrid = GetActiveGrid();
+        if (activeGrid.grid == null) return false; // falls kein Grid gefunden wurde abbrechen
+
+        activeGrid.grid.GetXY(GetMouseWorldPosition(), out int x, out int z);
         Vector2Int placedObjectOrigin = new Vector2Int(x, z);
         List<Vector2Int> gridPositionList = placedObjectTypeSO.GetGridPositionList(placedObjectOrigin, dir);
 
@@ -75,10 +100,10 @@ public class GridBuildingSystem : MonoBehaviour
 
         bool canBuild = true;
         foreach (Vector2Int gridPosition in gridPositionList) {
-            if (!grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
+            if (!activeGrid.grid.GetGridObject(gridPosition.x, gridPosition.y).CanBuild()) {
                 //Cannot build here
                 canBuild = false;
-                towerToUpgrade = grid.GetGridObject(gridPosition.x, gridPosition.y).GetPlacedObject().gameObject.GetComponent<BaseTower>();
+                towerToUpgrade = activeGrid.grid.GetGridObject(gridPosition.x, gridPosition.y).GetPlacedObject().gameObject.GetComponent<BaseTower>();
                 if (towerToUpgrade == null) {
                     Debug.LogError("towerToUpgrade is null on position: " + gridPosition.ToString());
                 }
@@ -91,8 +116,8 @@ public class GridBuildingSystem : MonoBehaviour
         {
 
             Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
-            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, z) +
-                new Vector3(rotationOffset.x, rotationOffset.y) * grid.GetCellSize();
+            Vector3 placedObjectWorldPosition = activeGrid.grid.GetWorldPosition(x, z) +
+                new Vector3(rotationOffset.x, rotationOffset.y) * activeGrid.grid.GetCellSize();
 
             if (!placedObjectTypeSO.prefab.gameObject.GetComponentInChildren<BaseTower>().enabled) {
                 placedObjectTypeSO.prefab.gameObject.GetComponentInChildren<BaseTower>().enabled = true;
@@ -102,7 +127,7 @@ public class GridBuildingSystem : MonoBehaviour
             placedObject.transform.rotation = Quaternion.Euler(0, 0, -placedObjectTypeSO.GetRotationAngle(dir));
 
             foreach (Vector2Int gridPosition in gridPositionList) {
-                grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
+                activeGrid.grid.GetGridObject(gridPosition.x, gridPosition.y).SetPlacedObject(placedObject);
             }
             DeselectObjectType();
             OnObjectPlaced?.Invoke(this, EventArgs.Empty);
@@ -125,6 +150,23 @@ public class GridBuildingSystem : MonoBehaviour
         return canBuild;
     }
 
+    private GridData GetActiveGrid()
+    {
+        if (Input.mousePosition.x < 200) // Beispielbedingung: Linke Seite des Bildschirms
+        {
+            return upperGrid;
+        }
+        else if (Input.mousePosition.x > 200)
+        {
+            return mainGrid;
+        }
+        else
+        {
+            return lowerGrid;
+        }
+        return new GridData();
+    }
+
     public static Vector3 GetMouseWorldPosition() {
         Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
         vec.z = 0f;
@@ -137,13 +179,16 @@ public class GridBuildingSystem : MonoBehaviour
     }
 
 
-    public Vector3 GetMouseWorldSnappedPosition() {
+    public Vector3 GetMouseWorldSnappedPosition() 
+    {
+        GridData activeGrid = GetActiveGrid();
+
         Vector3 mousePosition = GetMouseWorldPosition();
-        grid.GetXY(mousePosition, out int x, out int y);
+        activeGrid.grid.GetXY(mousePosition, out int x, out int y);
 
         if (placedObjectTypeSO != null) {
             Vector2Int rotationOffset = placedObjectTypeSO.GetRotationOffset(dir);
-            Vector3 placedObjectWorldPosition = grid.GetWorldPosition(x, y) + new Vector3(rotationOffset.x, rotationOffset.y) * grid.GetCellSize();
+            Vector3 placedObjectWorldPosition = activeGrid.grid.GetWorldPosition(x, y) + new Vector3(rotationOffset.x, rotationOffset.y) * activeGrid.grid.GetCellSize();
             return placedObjectWorldPosition;
         } else {
             return mousePosition;
@@ -177,6 +222,40 @@ public class GridBuildingSystem : MonoBehaviour
 
     public PlacedObjectTypeSO GetPlacedObjectTypeSO() {
         return placedObjectTypeSO;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (mainGrid.grid != null)
+        {
+            DrawGridGizmos(mainGrid);
+        }
+        if (upperGrid.grid != null)
+        {
+            DrawGridGizmos(upperGrid);
+        }
+        if (lowerGrid.grid != null)
+        {
+            DrawGridGizmos(lowerGrid);
+        }
+    }
+
+    private void DrawGridGizmos(GridData gridData)
+    {
+        Gizmos.color = Color.white; // Farbe des Gitters
+        Vector3 offset = gridData.grid.GetWorldPosition(0, 0) - transform.position; // Offset des Grids relativ zum Transform des Scripts
+
+        for (int x = 0; x <= gridData.width; x++)
+        {
+            for (int y = 0; y <= gridData.height; y++)
+            {
+                Vector3 worldPos = gridData.grid.GetWorldPosition(x, y);
+                Vector3 gizmosPos = new Vector3(worldPos.x, worldPos.y, 0f) + offset; // Z-Koordinate auf 0 setzen und Offset berücksichtigen
+
+                Gizmos.DrawLine(gizmosPos, gizmosPos + Vector3.right * gridData.grid.GetCellSize());
+                Gizmos.DrawLine(gizmosPos, gizmosPos + Vector3.up * gridData.grid.GetCellSize()); // Verwende Vector3.up für 2D
+            }
+        }
     }
 
 }
