d4c8142e3a7d9f8dba93240ebdc581cf3ec78921
Maximilian Fechner <iskalder.gg@gmail.com>
2025-02-01 15:45:13 +0100

    OwnGrid Initial

---
+++ Assets/02_Scripts/Map/GridCells.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GridCells : MonoBehaviour
+{
+    public int cellIndex;
+    public GameObject placedTower;
+    public string towerName;
+    public GameObject buildFoundation; //um evtl. ein Fundament zu aktivieren beim Platzieren etc
+
+    public bool isCellBuilt => placedTower != null; //check ob die Cell bereits bebaut ist
+
+    public void PlaceTower(GameObject tower)
+    {
+        placedTower = tower;
+        towerName = tower.name; // Name für spätere Vergleiche speichern
+    }
+}
